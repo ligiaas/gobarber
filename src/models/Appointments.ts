40a2f2370860847b68c51eb9f38b176a1202efd0
LIGIA DE ALMEIDA SILVA <38623489+ligiaas@users.noreply.github.com>
2020-09-27 14:41:01 -0300

    Insert join ProviderId in to Appointment Migration

--- src/models/Appointments.ts
+++ src/models/Appointments.ts
@@ -1,15 +1,38 @@
-import { Entity, Column, PrimaryGeneratedColumn } from 'typeorm'
+/* eslint-disable camelcase */
+import {
+  Entity,
+  Column,
+  PrimaryGeneratedColumn,
+  CreateDateColumn,
+  UpdateDateColumn,
+  ManyToOne,
+  JoinColumn,
+} from 'typeorm';
+import User from './User';
 
+/* P/ usar o @ vc tem que ir no tsconfig.json e mudar as vars
+ *  "emitDecoratorMetadata" e "experimentalDecorators" pra true e descomentar
+ */
 @Entity('appointments')
 class Appointment {
   @PrimaryGeneratedColumn('uuid')
   id: string;
 
   @Column()
-  provider: string;
+  providerId: string;
+
+  @ManyToOne(() => User)
+  @JoinColumn({ name: 'providerId' })
+  provider: User;
 
   @Column('timestamp with time zone')
   date: Date;
+
+  @CreateDateColumn()
+  createdAt: Date;
+
+  @UpdateDateColumn()
+  updatedAt: Date;
 }
 
 export default Appointment;
