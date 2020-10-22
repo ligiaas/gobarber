aeb375de1e8472913e7c7d2e8bff45564575569a
LIGIA DE ALMEIDA SILVA <38623489+ligiaas@users.noreply.github.com>
2020-10-21 22:00:40 -0300

    create test createAppointment

---
+++ src/modules/appointments/repositories/fakes/fakeAppointmentsRepository.ts
@@ -0,0 +1,32 @@
+import { uuid } from 'uuidv4';
+import IAppointmentsRepository from '@modules/appointments/repositories/IAppointmentsRepository';
+import ICreateAppointmentDTO from '@modules/appointments/dtos/ICreateAppointmentDTO';
+
+import Appointment from '@modules/appointments/infra/typeorm/entities/Appointment';
+
+class AppointmentsRepository implements IAppointmentsRepository {
+  private appointments: Appointment[] = [];
+
+  public async findByDate(date: Date): Promise<Appointment | undefined> {
+    const findAppointment = this.appointments.find(
+      appointment => appointment.date === date,
+    );
+
+    return findAppointment;
+  }
+
+  public async create({
+    providerId,
+    date,
+  }: ICreateAppointmentDTO): Promise<Appointment> {
+    const appointment = new Appointment();
+
+    Object.assign(appointment, { id: uuid(), date, providerId });
+
+    this.appointments.push(appointment);
+
+    return appointment;
+  }
+}
+
+export default AppointmentsRepository;
---
+++ src/modules/appointments/services/CreateAppointmentService.spec.ts
@@ -0,0 +1,23 @@
+import FakeAppointmentsRepository from '../repositories/fakes/fakeAppointmentsRepository';
+import CreateAppointmentService from './CreateAppointmentService';
+
+describe('CreateAppointment', () => {
+  it('should be able to create a new appointment', async () => {
+    const fakeAppointmentsRepository = new FakeAppointmentsRepository();
+    const createAppointment = new CreateAppointmentService(
+      fakeAppointmentsRepository,
+    );
+
+    const appointment = await createAppointment.execute({
+      date: new Date(),
+      providerId: '123123',
+    });
+
+    expect(appointment).toHaveProperty('id');
+    expect(appointment.providerId).toBe('123123');
+  });
+
+  // it('should not be able to create two appointments on the same time', () => {
+  //   expect(1 + 2).toBe(3);
+  // });
+});
