b02e92eff0af289546652b6724aaea89adbbd689
LIGIA DE ALMEIDA SILVA <38623489+ligiaas@users.noreply.github.com>
2020-10-24 01:41:40 -0300

    TDD forgot password with sendMail and generateToken functionalities

--- src/shared/container/providers/index.ts
+++ src/shared/container/providers/index.ts
@@ -2,8 +2,14 @@ import { container } from 'tsyringe';
 
 import IStorageProvider from './StorageProvider/models/IStorageProvider';
 import DiskStorageProvider from './StorageProvider/implementations/DiskStorageProvider';
+// import IMailProvider from './MailProvider/models/IMailProvider';
 
 container.registerSingleton<IStorageProvider>(
   'StorageProvider',
   DiskStorageProvider,
 );
+
+// container.registerSingleton<IMailProvider>(
+//   'IMailProvider',
+//   DiskStorageProvider,
+// );
